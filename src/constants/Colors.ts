ca3f58ea68445fb243db1d63745fc9605f254c86
Ege Barış Akyürek <72935668+degeloper34@users.noreply.github.com>
2022-07-05 21:22:51 +0300

    Colors added, Theme and useColorScheme removed

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,19 +1,20 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+  GREYISH_BROWN_30: "rgba(111, 93, 80, 0.3)",
+  SECONDARY_BUTTON_BACKGROUND: "#ececec",
+  GREYISH_BROWN_30_20: "rgba(111, 93, 80, 0.2)",
+  GREYISH_BROWN: "#434343",
+  BACKGROUND_GRAY: "#434343",
+  AQUA_GREEN: "#434343",
+  TOMATO: "#434343",
+  WHITE_TWO: "#434343",
+  WHITE_80: "rgba(242, 242, 242, 0.8)",
+  BLACK: "#000000",
+  ORANGE_RED: "#ff3b30",
+  BRIGHT_BLUE: "#007aff",
+  EGGPLANT_40: "rgba(4, 4, 15, 0.4)",
+  GREYISH_40: "rgba(176, 176, 176, 0.4)",
+  WHITE: "#ffffff",
+  MAIZE: "#f3c442",
+  ORANGISH: "#fa7745",
+  GREEN_TEAL: "#11b777",
 };
--- src/screens/TabOneScreen.tsx
+++ src/screens/TabOneScreen.tsx
@@ -1,7 +1,7 @@
 import {StyleSheet} from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
-import {Text, View} from "../components/Themed";
+import {Text, View} from "react-native";
 import {RootTabScreenProps} from "../../types";
 
 export default function TabOneScreen({
@@ -10,11 +10,7 @@ export default function TabOneScreen({
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+      <View style={styles.separator} />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
